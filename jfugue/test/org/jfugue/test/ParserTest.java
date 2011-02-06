a6b0de0ad030c47d717a548679f66f592398c21b
paul.butcher@gmail.com <paul.butcher@gmail.com@4e7ddc10-1079-11df-a8dc-870042ab4e57>
2011-02-06 10:01:22 +0000

    Added 'zero-one-many' tests for all event firing tests.
    git-svn-id: http://jfugue.googlecode.com/svn/trunk@87 4e7ddc10-1079-11df-a8dc-870042ab4e57

--- jfugue/test/org/jfugue/test/ParserTest.java
+++ jfugue/test/org/jfugue/test/ParserTest.java
@@ -140,10 +140,17 @@ public class ParserTest {
     }
 
     private ParserListener getNotifiedListener(final String methodName, final Object arg) {
-        ParserListener mockListener = mock(ParserListener.class);
-        parser.addParserListener(mockListener);
+	return getNotifiedListeners(1, methodName, arg)[0];
+    }
+
+    private ParserListener [] getNotifiedListeners(final int listenerCount, final String methodName, final Object arg) {
+        ParserListener [] mockListeners = new ParserListener[listenerCount];
+	for(int i = 0; i < listenerCount; ++i) {
+	    mockListeners[i] = mock(ParserListener.class);
+	    parser.addParserListener(mockListeners[i]);
+	}
 	callEventMethod(methodName,arg);
-	return mockListener;
+	return mockListeners;
     }
 
     private void callEventMethod(final String methodName, final Object arg) {
@@ -171,93 +178,317 @@ public class ParserTest {
     }
     
     @Test 
-    public void testfireVoiceElement() {
+    public void testfireVoiceEvent_Zero() {
+	final Voice element = new Voice((byte)0);
+        getNotifiedListeners(0,"fireVoiceEvent", element);
+    }
+
+    @Test 
+    public void testfireVoiceEvent_One() {
 	final Voice element = new Voice((byte)0);
         verify(getNotifiedListener("fireVoiceEvent", element)).voiceEvent(element);	
     }
 
     @Test 
-    public void testfireTempoEvent() {
+    public void testfireVoiceEvent_Many() {
+	final Voice element = new Voice((byte)0);
+	ParserListener [] listeners = getNotifiedListeners(3, "fireVoiceEvent", element);
+	for (int i = 0; i < 3; ++i) {
+	    verify(listeners[i]).voiceEvent(element);
+	}
+    }
+
+    @Test 
+    public void testfireTempoEvent_Zero() {
+	final Tempo element = new Tempo(0);
+        getNotifiedListeners(0,"fireTempoEvent", element);
+    }
+
+    @Test 
+    public void testfireTempoEvent_One() {
 	final Tempo element = new Tempo(0);
         verify(getNotifiedListener("fireTempoEvent", element)).tempoEvent(element);
     }
 
     @Test 
-    public void testfireInstrumentEvent() {
+    public void testfireTempoEvent_Many() {
+	final Tempo element = new Tempo(0);
+	ParserListener [] listeners = getNotifiedListeners(3,"fireTempoEvent", element);
+	for (int i = 0; i < 3; ++i) {
+	    verify(listeners[i]).tempoEvent(element);
+	}
+    }
+
+    @Test 
+    public void testfireInstrumentEvent_Zero() {
+	final Instrument element = new Instrument((byte)0);
+        getNotifiedListeners(0,"fireInstrumentEvent", element);
+    }
+
+    @Test 
+    public void testfireInstrumentEvent_One() {
 	final Instrument element = new Instrument((byte)0);
         verify(getNotifiedListener("fireInstrumentEvent", element)).instrumentEvent(element);
     }
 
     @Test 
-    public void testfireLayerEvent() {
+    public void testfireInstrumentEvent_Many() {
+	final Instrument element = new Instrument((byte)0);
+	ParserListener [] listeners = getNotifiedListeners(3, "fireInstrumentEvent", element);
+	for (int i = 0; i < 3; ++i) {
+	    verify(listeners[i]).instrumentEvent(element);
+	}
+    }
+
+    @Test 
+    public void testfireLayerEvent_Zero() {
+	final Layer element = new Layer((byte)0);
+        getNotifiedListeners(0,"fireLayerEvent", element);
+    }
+
+    @Test 
+    public void testfireLayerEvent_One() {
 	final Layer element = new Layer((byte)0);
         verify(getNotifiedListener("fireLayerEvent", element)).layerEvent(element);
     }
 
     @Test 
-    public void testfireTimeEvent() {
+    public void testfireLayerEvent_Many() {
+	final Layer element = new Layer((byte)0);
+	ParserListener [] listeners = getNotifiedListeners(3, "fireLayerEvent", element);
+	for (int i = 0; i < 3; ++i) {
+	    verify(listeners[i]).layerEvent(element);
+	}
+    }
+
+    @Test
+    public void testfireTimeEvent_Zero() {
+	final Time element = new Time(0);
+        getNotifiedListeners(0,"fireTimeEvent", element);
+    }
+
+    @Test
+    public void testfireTimeEvent_One() {
 	final Time element = new Time(0);
         verify(getNotifiedListener("fireTimeEvent", element)).timeEvent(element);
     }
 
+    @Test
+    public void testfireTimeEvent_Many() {
+	final Time element = new Time(0);
+	ParserListener [] listeners = getNotifiedListeners(3, "fireTimeEvent", element);
+	for (int i = 0; i < 3; ++i) {
+	    verify(listeners[i]).timeEvent(element);
+	}
+    }
+
     @Test 
-    public void testfireSystemExclusiveEvent() {
+    public void testfireSystemExclusiveEvent_Zero() {
+	final SystemExclusiveEvent element = new SystemExclusiveEvent(null);
+        getNotifiedListeners(0,"fireSystemExclusiveEvent", element);
+    }    
+
+    @Test 
+    public void testfireSystemExclusiveEvent_One() {
 	final SystemExclusiveEvent element = new SystemExclusiveEvent(null);
         verify(getNotifiedListener("fireSystemExclusiveEvent", element)).systemExclusiveEvent(element);
     }    
 
     @Test 
-    public void testfireKeySignatureEvent() {
+    public void testfireSystemExclusiveEvent_Many() {
+	final SystemExclusiveEvent element = new SystemExclusiveEvent(null);
+	ParserListener [] listeners = getNotifiedListeners(3, "fireSystemExclusiveEvent", element);
+	for (int i = 0; i < 3; ++i) {
+	    verify(listeners[i]).systemExclusiveEvent(element);
+	}
+    }    
+
+    @Test
+    public void testfireKeySignatureEvent_Zero() {
+	final KeySignature element = new KeySignature((byte)0, (byte)0);
+        getNotifiedListeners(0,"fireKeySignatureEvent", element);
+    }
+
+    @Test
+    public void testfireKeySignatureEvent_One() {
 	final KeySignature element = new KeySignature((byte)0, (byte)0);
         verify(getNotifiedListener("fireKeySignatureEvent", element)).keySignatureEvent(element);
     }
 
+    @Test
+    public void testfireKeySignatureEvent_Many() {
+	final KeySignature element = new KeySignature((byte)0, (byte)0);
+	ParserListener [] listeners = getNotifiedListeners(3, "fireKeySignatureEvent", element);
+	for (int i = 0; i < 3; ++i) {
+	    verify(listeners[i]).keySignatureEvent(element);
+	}
+    }
+
     @Test 
-    public void testfireMeasureEvent() {
+    public void testfireMeasureEvent_Zero() {
+	final Measure element = new Measure();
+        getNotifiedListeners(0,"fireMeasureEvent", element);
+    }
+
+    @Test 
+    public void testfireMeasureEvent_One() {
 	final Measure element = new Measure();
         verify(getNotifiedListener("fireMeasureEvent", element)).measureEvent(element);
     }
 
     @Test 
-    public void testfireControllerEvent() {
+    public void testfireMeasureEvent_Many() {
+	final Measure element = new Measure();
+	ParserListener [] listeners = getNotifiedListeners(3, "fireMeasureEvent", element);
+	for (int i = 0; i < 3; ++i) {
+	    verify(listeners[i]).measureEvent(element);
+	}
+    }
+
+    @Test 
+    public void testfireControllerEvent_Zero() {
+	final Controller element = new Controller();
+        getNotifiedListeners(0,"fireControllerEvent", element);
+    }
+
+    @Test 
+    public void testfireControllerEvent_One() {
 	final Controller element = new Controller();
         verify(getNotifiedListener("fireControllerEvent", element)).controllerEvent(element);
     }
 
     @Test 
-    public void testfireChannelPressureEvent() {
+    public void testfireControllerEvent_Many() {
+	final Controller element = new Controller();
+	ParserListener [] listeners = getNotifiedListeners(3, "fireControllerEvent", element);
+	for (int i = 0; i < 3; ++i) {
+	    verify(listeners[i]).controllerEvent(element);
+	}
+    }
+
+    @Test 
+    public void testfireChannelPressureEvent_Zero() {
+	final ChannelPressure element = new ChannelPressure((byte)0);
+        getNotifiedListeners(0,"fireChannelPressureEvent", element);
+    }
+
+    @Test 
+    public void testfireChannelPressureEvent_One() {
 	final ChannelPressure element = new ChannelPressure((byte)0);
         verify(getNotifiedListener("fireChannelPressureEvent", element)).channelPressureEvent(element);
     }
 
     @Test 
-    public void testfirePolyphonicPressureEvent() {
+    public void testfireChannelPressureEvent_Many() {
+	final ChannelPressure element = new ChannelPressure((byte)0);
+	ParserListener [] listeners = getNotifiedListeners(3, "fireChannelPressureEvent", element);
+	for (int i = 0; i < 3; ++i) {
+	    verify(listeners[i]).channelPressureEvent(element);
+	}
+    }
+
+    @Test 
+    public void testfirePolyphonicPressureEvent_Zero() {
+	final PolyphonicPressure element = new PolyphonicPressure((byte)0, (byte)0);
+        getNotifiedListeners(0,"firePolyphonicPressureEvent", element);
+    }
+    
+    @Test 
+    public void testfirePolyphonicPressureEvent_One() {
 	final PolyphonicPressure element = new PolyphonicPressure((byte)0, (byte)0);
         verify(getNotifiedListener("firePolyphonicPressureEvent", element)).polyphonicPressureEvent(element);
     }
     
     @Test 
-    public void testfirePitchBendEvent() {
+    public void testfirePolyphonicPressureEvent_Many() {
+	final PolyphonicPressure element = new PolyphonicPressure((byte)0, (byte)0);
+	ParserListener [] listeners = getNotifiedListeners(3, "firePolyphonicPressureEvent", element);
+	for (int i = 0; i < 3; ++i) {
+	    verify(listeners[i]).polyphonicPressureEvent(element);
+	}
+    }
+    
+    @Test 
+    public void testfirePitchBendEvent_Zero() {
+	final PitchBend element = new PitchBend((byte)0, (byte)0);
+        getNotifiedListeners(0,"firePitchBendEvent", element);
+    }    
+
+    @Test 
+    public void testfirePitchBendEvent_One() {
 	final PitchBend element = new PitchBend((byte)0, (byte)0);
         verify(getNotifiedListener("firePitchBendEvent", element)).pitchBendEvent(element);
     }    
 
     @Test 
-    public void testfireNoteEvent() {
+    public void testfirePitchBendEvent_Many() {
+	final PitchBend element = new PitchBend((byte)0, (byte)0);
+	ParserListener [] listeners = getNotifiedListeners(3, "firePitchBendEvent", element);
+	for (int i = 0; i < 3; ++i) {
+	    verify(listeners[i]).pitchBendEvent(element);
+	}
+    }    
+
+    @Test 
+    public void testfireNoteEvent_Zero() {
+	final Note element = new Note((byte)0);
+        getNotifiedListeners(0,"fireNoteEvent", element);
+    }
+
+    @Test 
+    public void testfireNoteEvent_One() {
 	final Note element = new Note((byte)0);
         verify(getNotifiedListener("fireNoteEvent", element)).noteEvent(element);
     }
 
     @Test 
-    public void testfireSequentialNoteEvent() {
+    public void testfireNoteEvent_Many() {
+	final Note element = new Note((byte)0);
+	ParserListener [] listeners = getNotifiedListeners(3, "fireNoteEvent", element);
+	for (int i = 0; i < 3; ++i) {
+	    verify(listeners[i]).noteEvent(element);
+	}
+    }
+
+    @Test 
+    public void testfireSequentialNoteEvent_Zero() {
+	final Note element = new Note((byte)0);
+        getNotifiedListeners(0,"fireSequentialNoteEvent", element);
+    }
+
+    public void testfireSequentialNoteEvent_One() {
 	final Note element = new Note((byte)0);
         verify(getNotifiedListener("fireSequentialNoteEvent", element)).sequentialNoteEvent(element);
     }
 
     @Test 
-    public void testfireParallelNoteEvent() {
+    public void testfireSequentialNoteEvent_Many() {
+	final Note element = new Note((byte)0);
+	ParserListener [] listeners = getNotifiedListeners(3, "fireSequentialNoteEvent", element);
+	for (int i = 0; i < 3; ++i) {
+	    verify(listeners[i]).sequentialNoteEvent(element);
+	}
+    }
+
+    @Test
+    public void testfireParallelNoteEvent_Zero() {
+	final Note element = new Note((byte)0);
+        getNotifiedListeners(0,"fireParallelNoteEvent", element);
+    }
+
+    @Test
+    public void testfireParallelNoteEvent_One() {
 	final Note element = new Note((byte)0);
         verify(getNotifiedListener("fireParallelNoteEvent", element)).parallelNoteEvent(element);
+    }
+    
+    @Test
+    public void testfireParallelNoteEvent_Many() {
+	final Note element = new Note((byte)0);
+	ParserListener [] listeners = getNotifiedListeners(3, "fireParallelNoteEvent", element);
+	for (int i = 0; i < 3; ++i) {
+	    verify(listeners[i]).parallelNoteEvent(element);
+	}
     }
 
 }
