6ae8000705ae1eafabe3509e424243641cf02a72
dmkoelle <dmkoelle@4e7ddc10-1079-11df-a8dc-870042ab4e57>
2011-02-09 04:30:55 +0000

    Updated code with new location of Measure in org.jfugue.elements package. Also reformatted to convert tabs to spaces.
    git-svn-id: http://jfugue.googlecode.com/svn/trunk@97 4e7ddc10-1079-11df-a8dc-870042ab4e57

--- jfugue/test/org/jfugue/test/MeasureTest.java
+++ jfugue/test/org/jfugue/test/MeasureTest.java
@@ -3,63 +3,62 @@ package org.jfugue.test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import org.junit.Before;
-import org.junit.Test;
-
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-import org.jfugue.Measure;
-import org.jfugue.ElementVisitor;
+import org.jfugue.elements.ElementVisitor;
+import org.jfugue.elements.Measure;
+import org.junit.Before;
+import org.junit.Test;
 
 public class MeasureTest {
     private Measure measure;
 
     @Before
     public void setUp() {
-	measure = new Measure();
+        measure = new Measure();
     }
 
     @Test
     public void testGetMusicString() {
-	assertEquals("|", measure.getMusicString());
+        assertEquals("|", measure.getMusicString());
     }
 
     @Test
     public void testGetVerifyString() {
-	assertEquals("Measure", measure.getVerifyString());
+        assertEquals("Measure", measure.getVerifyString());
     }
 
     @Test
-    public void testAcceptVisitor(){
-	ElementVisitor v = mock(ElementVisitor.class);
-	measure.acceptVisitor(v);
-	verify(v).visit(measure);
+    public void testAcceptVisitor() {
+        ElementVisitor v = mock(ElementVisitor.class);
+        measure.acceptVisitor(v);
+        verify(v).visit(measure);
     }
 
     @Test
-    public void testEqualsNull(){
-	assertFalse(measure.equals(null));
+    public void testEqualsNull() {
+        assertFalse(measure.equals(null));
     }
 
     @Test
-    public void testEqualsSomethingElse(){
-	assertFalse(measure.equals(new String()));
+    public void testEqualsSomethingElse() {
+        assertFalse(measure.equals(new String()));
     }
 
     @Test
-    public void testEqualsSelf(){
-	assertTrue(measure.equals(measure));
+    public void testEqualsSelf() {
+        assertTrue(measure.equals(measure));
     }
 
     @Test
-    public void testEqualsADifferentMeasure(){
-	//Reflects behaviour at time of writing test.
-	assertTrue(measure.equals(new Measure()));
+    public void testEqualsADifferentMeasure() {
+        // Reflects behaviour at time of writing test.
+        assertTrue(measure.equals(new Measure()));
     }
 
     @Test
     public void testHashCode() {
-	assertEquals(201, measure.hashCode());
+        assertEquals(201, measure.hashCode());
     }
 }
